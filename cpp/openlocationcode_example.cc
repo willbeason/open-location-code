3773184cf1009117c4b4f45d6024706ee371b284
Will Beason <willbeason@gmail.com>
2026-03-09 13:56:54 -0500

    Fix issues in openlocationcode_example.cc

--- cpp/openlocationcode_example.cc
+++ cpp/openlocationcode_example.cc
@@ -15,7 +15,7 @@ int main() {
   // => "8FVC2222+22GCCCC"
 
   // Decodes a Plus+Code back into coordinates.
-  openlocationcode::CodeArea code_area = openlocationcode::Decode(code);
+  const openlocationcode::CodeArea code_area = openlocationcode::Decode(code);
   std::cout << "Code length: " << std::fixed << std::setprecision(15) << ' '
             << code_area.GetLatitudeLo()          // 47.000062479999997
             << ' ' << code_area.GetLongitudeLo()  // 8.000062500000013
@@ -25,29 +25,29 @@ int main() {
             << std::endl;
 
   // Checks if a Plus+Code is valid.
-  bool isValid = openlocationcode::IsValid(code);
+  const bool isValid = openlocationcode::IsValid(code);
   std::cout << "Is valid? " << isValid << std::endl;
   // => true
 
   // Checks if a Plus+Code is full.
-  bool isFull = openlocationcode::IsFull(code);
+  const bool isFull = openlocationcode::IsFull(code);
   std::cout << "Is full? " << isFull << std::endl;
   // => true
 
   // Checks if a Plus+Code is short.
-  bool isShort = openlocationcode::IsShort(code);
+  const bool isShort = openlocationcode::IsShort(code);
   std::cout << "Is short? " << isShort << std::endl;
   // => false
 
   // Shorten a Plus+Codes if possible by the given reference latitude and
   // longitude.
-  std::string short_code =
+  const std::string short_code =
       openlocationcode::Shorten("9C3W9QCJ+2VX", {51.3708675, -1.217765625});
   std::cout << "Shortened: " << short_code << std::endl;
   // => "CJ+2VX"
 
   // Extends a Plus+Code by the given reference latitude and longitude.
-  std::string recovered_code =
+  const std::string recovered_code =
       openlocationcode::RecoverNearest("CJ+2VX", {51.3708675, -1.217765625});
   std::cout << "Recovered: " << recovered_code << std::endl;
   // => "9C3W9QCJ+2VX"
